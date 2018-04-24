935564f4d0e5ad871aedba4820f859fe113d3d07
Shark <thaynie@gmail.com>
2018-04-23 20:44:07 -0500

    initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { Shard01Component } from './shards/shard-01/shard-01.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Shard01Component
   ],
   imports: [
     BrowserModule
---
+++ src/app/shards/shard-01/shard-01.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-shard-01',
+  templateUrl: './shard-01.component.html',
+  styleUrls: ['./shard-01.component.css']
+})
+export class Shard01Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
